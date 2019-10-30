81078308100f1758371ff09dfb3eaff95578b695
outstanding-reference-3248 <omar.benjamin377@gmail.com>
2019-10-30 03:36:20 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,13 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  say = "It's Dangerous To Go Alone! Take This."
+  puts say
+end
+
+def any_phrase
+  seas = "Do A Barrel Roll!"
+  puts seas
+end  
